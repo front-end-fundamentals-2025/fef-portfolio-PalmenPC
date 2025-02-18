e545cc2dd9416e7909299d0e7443f89df2b8196e
Emil Eriksson <98emil.eriksson@gmail.com>
2025-02-18 20:27:07 +0100

    Added further functionality to theme button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const themeButton = document.getElementById("theme-button");
 const bodyElement = document.querySelector("body");
+const headerElement = document.querySelector("header");
+const footerElement = document.querySelector("footer");
 
 themeButton.addEventListener("click", () => {
   bodyElement.classList.toggle("light-mode");
+  headerElement.classList.toggle("light-mode-navigation");
+  footerElement.classList.toggle("light-mode-navigation");
 });
