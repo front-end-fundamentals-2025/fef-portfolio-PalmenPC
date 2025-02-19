4535cc774826cb6d7dfef90ce0d09ad3cc4c34ee
Emil Eriksson <98emil.eriksson@gmail.com>
2025-02-19 16:28:27 +0100

    Changes to the theme button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+/* Theme Button */
 const themeButton = document.getElementById("theme-button");
-const bodyElement = document.querySelector("body");
 const headerElement = document.querySelector("header");
+const bodyElement = document.querySelector("body");
 const footerElement = document.querySelector("footer");
 
 themeButton.addEventListener("click", () => {
