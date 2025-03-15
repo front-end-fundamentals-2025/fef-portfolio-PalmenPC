e838fe832cf3189c1a7e71db634afa3fe65e1f1e
Emil Eriksson <98emil.eriksson@gmail.com>
2025-03-15 20:09:17 +0100

    Used localStorage to remember theme preference

--- js/main.js
+++ js/main.js
@@ -4,8 +4,33 @@ const headerElement = document.querySelector("header");
 const bodyElement = document.querySelector("body");
 const footerElement = document.querySelector("footer");
 
+function lightMode() {
+  localStorage.theme = "light";
+  bodyElement.classList.add("light-mode");
+  headerElement.classList.add("light-mode-navigation");
+  footerElement.classList.add("light-mode-navigation");
+}
+
+function darkMode() {
+  localStorage.theme = "dark";
+  bodyElement.classList.remove("light-mode");
+  headerElement.classList.remove("light-mode-navigation");
+  footerElement.classList.remove("light-mode-navigation");
+}
+
+/* Retrieves the previously stored theme */
+let theme = localStorage.getItem("theme");
+
+if (localStorage.theme === "dark") {
+  darkMode();
+} else if (localStorage.theme === "light") {
+  lightMode();
+}
+
 themeButton.addEventListener("click", () => {
-  bodyElement.classList.toggle("light-mode");
-  headerElement.classList.toggle("light-mode-navigation");
-  footerElement.classList.toggle("light-mode-navigation");
+  if (localStorage.theme === "dark") {
+    lightMode();
+  } else if (localStorage.theme === "light") {
+    darkMode();
+  }
 });
